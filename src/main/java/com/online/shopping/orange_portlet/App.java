241f0d7bbdfe44d73c35c26db2aa3d0c2b836461
cainzhong <cainzhong@aliyun.com>
2015-03-19 22:47:22 +0800

    create project

---
+++ src/main/java/com/online/shopping/orange_portlet/App.java
@@ -0,0 +1,13 @@
+package com.online.shopping.orange_portlet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
