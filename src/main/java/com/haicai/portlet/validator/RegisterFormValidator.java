a279adc8d134a82b12c046578bd96f7ab08e9de7
Jason <Jason@Jason-PC>
2015-05-20 00:26:37 +0800

    Update RegisterFormValidator class

--- src/main/java/com/haicai/portlet/validator/RegisterFormValidator.java
+++ src/main/java/com/haicai/portlet/validator/RegisterFormValidator.java
@@ -3,11 +3,14 @@ package com.haicai.portlet.validator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.binding.message.MessageBuilder;
 import org.springframework.binding.message.MessageContext;
 import org.springframework.binding.validation.ValidationContext;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 import com.haicai.portlet.form.RegisterForm;
 
@@ -20,34 +23,47 @@ import com.haicai.portlet.form.RegisterForm;
 @Component
 public class RegisterFormValidator {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(RegisterFormValidator.class);
+	
 	public void validateRegister(RegisterForm registerForm, ValidationContext context){
 
         MessageContext messages = context.getMessageContext();
         String errorMsg = null;
        if(StringUtils.isEmpty(registerForm.getName())){
+    	   	  LOGGER.error("UserName is empty!");
               errorMsg = "T_ERROR";
               messages.addMessage(new MessageBuilder().error().code(errorMsg).build());
         }
        if(StringUtils.isEmpty(registerForm.getActname())){
+    	      LOGGER.error("Actual name is empty!");
               errorMsg = "T_ERROR";
               messages.addMessage(new MessageBuilder().error().code(errorMsg).build());
         }
        if(StringUtils.isEmpty(registerForm.getEngname())){
+    	      LOGGER.error("English name is empty!");
               errorMsg = "T_ERROR";
               messages.addMessage(new MessageBuilder().error().code(errorMsg).build());
         }
        if(StringUtils.isEmpty(registerForm.getGender())){
+    	      LOGGER.error("User's gender is empty!");
               errorMsg = "T_ERROR";
               messages.addMessage(new MessageBuilder().error().code(errorMsg).build());
         }
        if(StringUtils.isEmpty(registerForm.getCpassword())){
+    	      LOGGER.error("User's passwrod is empty!");
               errorMsg = "T_ERROR";
               messages.addMessage(new MessageBuilder().error().code(errorMsg).build());
         }
        if(StringUtils.isEmpty(registerForm.getEmail()) && this.isEmail(registerForm.getEmail())){
+    	      LOGGER.error("User's Email is empty!");
               errorMsg = "T_ERROR";
               messages.addMessage(new MessageBuilder().error().code(errorMsg).build());
        }
+       if(!NumberUtils.isNumber(registerForm.getPhoneno()) && StringUtils.isEmpty(registerForm.getPhoneno())){
+	    	   LOGGER.error("User's phone number is empty!");
+	           errorMsg = "T_ERROR";
+	           messages.addMessage(new MessageBuilder().error().code(errorMsg).build());
+    }
 }
        
        /**
